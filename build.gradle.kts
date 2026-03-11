20a407bafe2eab48129f12e571c21c3fa7865306
Proify <247336249+proify@users.noreply.github.com>
2026-03-11 14:15:53 +0800

    chore(version): 更新应用版本号
    - 将 appVersionCode 从 19 更新为 20
    - 将 appVersionName 从 1.0.30-beta1 更新为 1.0.30-beta2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,8 @@ plugins {
 }
 
 extra["appPackageName"] = "io.github.proify.lyricon"
-extra["appVersionCode"] = 19
-extra["appVersionName"] = "1.0.30-beta1"
+extra["appVersionCode"] = 20
+extra["appVersionName"] = "1.0.30-beta2"
 extra["compileSdkVersion"] = 36
 extra["targetSdkVersion"] = 36
 extra["minSdkVersion"] = 27
